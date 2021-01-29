5db0f99c175b5bb46b9a5ef13d43a165f28195a4
chrisjaechun <chrisjaechun@gmail.com>
2021-01-29 11:47:10 -0500

    Create Update form
    - Our update route was referencing our Create Form which has all fields
      required.
    -- We've copied the same exact form for update but removed the required
      property.

---
+++ src/components/Form/UpdateForm.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const UpdateForm = ({ handleSubmit, handleInputChange, item }) => (
+  <Form onSubmit={handleSubmit}>
+    <Form.Group controlId="name">
+      <Form.Label>Name</Form.Label>
+      <Form.Control
+        name="name"
+        type="text"
+        placeholder="Item name here"
+        value={item.name}
+        onChange={handleInputChange}
+      />
+    </Form.Group>
+    <Form.Group controlId="quantity">
+      <Form.Label>Quantity</Form.Label>
+      <Form.Control
+        name="quantity"
+        type="number"
+        placeholder="How many?"
+        value={item.quantity}
+        onChange={handleInputChange}
+      />
+    </Form.Group>
+    <Form.Group controlId="cost">
+      <Form.Label>Cost</Form.Label>
+      <Form.Control
+        name="cost"
+        type="number"
+        placeholder="How much to move?"
+        value={item.cost}
+        onChange={handleInputChange}
+      />
+    </Form.Group>
+    <Form.Group controlId="size">
+      <Form.Label>Size</Form.Label>
+      <Form.Control
+        name="size"
+        type="text"
+        placeholder="How big or small is it?"
+        value={item.size}
+        onChange={handleInputChange}
+      />
+    </Form.Group>
+    <Form.Group controlId="room">
+      <Form.Label>Room</Form.Label>
+      <Form.Control
+        name="room"
+        type="text"
+        placeholder="Where's this going?"
+        value={item.room}
+        onChange={handleInputChange}
+      />
+    </Form.Group>
+    <Form.Group controlId="category">
+      <Form.Label>Category</Form.Label>
+      <Form.Control
+        name="category"
+        type="text"
+        placeholder="Category"
+        value={item.category}
+        onChange={handleInputChange}
+      />
+    </Form.Group>
+    <Button
+      variant="primary"
+      type="submit"
+    >
+      Submit
+    </Button>
+  </Form>
+)
+
+export default UpdateForm
